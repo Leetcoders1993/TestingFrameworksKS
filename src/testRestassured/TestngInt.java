33edb7c1a83f8fa15b01061574ff1e85a6408ade
Sanjay.Adaki <adakisanjay@gmail.com>
2023-03-08 00:24:01 +0530

    this is the whereever house

---
+++ src/testRestassured/TestngInt.java
@@ -0,0 +1,17 @@
+package testRestassured;
+
+public class TestngInt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		//this is the new code that needs to be added in the main wherever needs to ad
+		//there were not toasts no loaded tables no one sung a song when we were born and 
+		//and our mothers at our cradles . when the night time they came they rocked us to their breasts and blessed us sleep my baby sleep mu son 
+		
+		
+
+	}
+
+}
--- src/testRestassured/interview.java
+++ src/testRestassured/interview.java
@@ -6,75 +6,10 @@ public class interview {
 		// TODO Auto-generated method stub
 		
 		
-		double intval = 123456;
 		
-		String one = "1";
-		String two = "2";
-		String three = "3";
-		String four = "4";
-		String five = "5";
-		
-		
-		String valueString = Double.toString(i);
-		
-		
-		
-		String word ="";
-		
-		
-		for(int i =0;i<valueString.length();i++) {
-			
-			if(valueString.length()==6) {
-				
-				if(i==0) {
-					
-					switch (valueString) {
-					case one:
-						word = word + "one Lakh";
-						
-						break;
-						
-					case two:
-						word = word + "two Lakh";
-						
-						break;
-						
-
-					case three:
-						word = word + "three Lakh";
-						
-						break;
-						
-						
-					}
-					
-					valueString.charAt(i)
-				}
-				
-				if(i==1) {
-					
-					String Key = String(valueString.charAt(i)+valueString.charAt(i+1));
-					
-					switch (key) {
-					case value:
-						
-						break;
-
-					default:
-						break;
-					}
-					
-					
-					
-					
-					
-				}
-				
-			}
 			
 			
-			
-		}
+		
 
 	}
 

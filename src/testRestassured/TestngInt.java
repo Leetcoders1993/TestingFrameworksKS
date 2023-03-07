1bd2a3852b87208a3eea5370e212b7bdf4ce372b
Leetcoders1993 <126816714+Leetcoders1993@users.noreply.github.com>
2023-03-08 00:30:41 +0530

    Update TestngInt.java

--- src/testRestassured/TestngInt.java
+++ src/testRestassured/TestngInt.java
@@ -7,8 +7,7 @@ public class TestngInt {
 		
 		
 		//this is the new code that needs to be added in the main wherever needs to ad
-		//there were not toasts no loaded tables no one sung a song when we were born and 
-		//and our mothers at our cradles . when the night time they came they rocked us to their breasts and blessed us sleep my baby sleep mu son 
+		//ther
 		
 		
 

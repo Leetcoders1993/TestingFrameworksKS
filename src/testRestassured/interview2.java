11136e091691b900c17ad83a2367ce27581baa21
Sanjay.Adaki <adakisanjay@gmail.com>
2023-03-09 00:02:47 +0530

    this is new

---
+++ src/testRestassured/interview2.java
@@ -0,0 +1,44 @@
+package testRestassured;
+
+public class interview2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		String str= "sanjay123adaki";
+		
+		for(int i=0;i<str.length();i++) {
+			
+			if(str.charAt(i)=='1'||str.charAt(i)=='2'||str.charAt(i)=='3') {
+				
+				System.out.println(str.charAt(i));
+				
+			}/*for(int i1=0;i1<s.length;i1++) {
+				
+				for(int j=0;j<t.length;j++) {
+					
+					if(s[i1]==t[j]) {
+						
+						System.out.println();
+						
+					}
+				}
+				*/
+				
+			}
+		
+		int[] s = {1,2,3,4,5};
+		int[] t = {4,5,8,6,7,9,2,3};
+		
+		
+		
+		
+		
+		
+			
+		}
+
+	}
+
+}
